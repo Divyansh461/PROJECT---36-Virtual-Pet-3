575d13d4bf6e8376b44a250d38664329aded6496
Divyansh461 <78966633+Divyansh461@users.noreply.github.com>
2021-08-24 23:13:18 +0530

    Add files via upload

---
+++ images/js/food.js
@@ -0,0 +1,59 @@
+class Food {
+    constructor() {
+
+
+     this.foodstock = 0;
+     this.lastfed;
+     this.image = loadImage("images/Milk.png");
+     
+    }
+
+updatefoodstock(foodstock) {
+    this.foodstock = foodstock;
+}
+
+getfedfime(lastfed){
+    this.lastfed = lastfed;
+}
+
+deductfood() {
+    if(this.foodstock > 0) {
+        this.foodstock = this.foodstock - 1;
+    }
+}
+
+getfoodstock() {
+    return this.foodstock;
+}
+
+display() {
+    var x = 80,y = 100;
+    
+    if(this.foodstock !== 0) {
+        for(var i =0;i < this.foodstock;i++) {
+            if(i % 10 === 0) {
+                x = 80;
+                y = y + 50;
+            }
+            imageMode(CENTER);
+            image (this.image,x,y,50,50);
+            x = x + 35;
+        }
+    }
+}
+ 
+bedroom() {
+    background(bedimg,500,300)
+}
+
+garden() {
+    background(gardenimg,500,300);
+}
+
+washroom() {
+    background(washroomimg,500,300);
+}
+
+
+    
+}
